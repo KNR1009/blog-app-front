0ef171d148558f4a3821bbc41ad1c781941dee05
伊東和成 <itokazunari5375@icloud.com>
2022-06-14 09:19:23 +0900

    骨格用のファイルを作成

---
+++ pages/blog/index.tsx
@@ -0,0 +1,13 @@
+// Types
+import { NextPage } from "next";
+import React from "react";
+
+// components
+
+type Props = {};
+
+const BlogPage: NextPage<Props> = () => {
+  return <h1>ブログページです</h1>;
+};
+
+export default BlogPage;
